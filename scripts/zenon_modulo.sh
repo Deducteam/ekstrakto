478a7eda757067c859f5cd2ac4709475d4abe36d
Yacine <elhaddadmohamedyacine@hotmail.com>
2021-05-11 04:40:51 +0200

    update script for zenon_modulo

--- scripts/zenon_modulo.sh
+++ scripts/zenon_modulo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NBJOBS=50
+NBJOBS=56
 
 function call_zenon() {
   INPUT="$1"
   FOLDER="`echo $INPUT|cut -d'/' -f 3`"
   FILE="`echo $INPUT|cut -d'/' -f 5`"
-  OUTPUT="files/proofs/$FOLDER/lemmas/$(basename $FILE .p).lp"
+  OUTPUT="~/yacine/proofs_eprover/$FOLDER/lemmas/$(basename $FILE .p).lp"
   if zenon_modulo -itptp -modulo -modulo-heuri -odkterm -sig $FOLDER -max-time \
     10s -max-size 2G $INPUT > $OUTPUT 2> /dev/null
   then
@@ -14,18 +14,11 @@ function call_zenon() {
   else
     echo -e "[zenonmodulo] Failure on file [$FOLDER][$(basename $INPUT)]"
     rm "$OUTPUT"
-    touch "files/proofs/$FOLDER/lemmas/$(basename $INPUT .p).failed"
+    touch "~/yacine/proofs_eprover/$FOLDER/lemmas/$(basename $INPUT .p).failed"
   fi
 }
 
 export -f call_zenon
 
-
-START_DATE="`date -R`"
-
-echo "Running zenonmodulo on $NB_OK trace..."
-
-find files/proofs -name *.p|
+find ~/yacine/proofs_eprover -name "*.p"|
     xargs -P $NBJOBS -n 1 -I{} bash -c "call_zenon {}"
-
-END_DATE="`date -R`"
